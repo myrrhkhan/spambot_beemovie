5439dcc37bf41ebbd1925eb91cb668808b823162
Myrrh Khan <myrrhkhan@gmail.com>
2020-11-24 00:10:36 -0500

    added escape key

--- yeetbot.py
+++ yeetbot.py
@@ -1,6 +1,8 @@
-import pyautogui, time
-time.sleep(10)
+import pyautogui, time, keyboard, cv2
+time.sleep(5)
 yeet = open("beemovie.txt", "r")
 for uh in yeet:
     pyautogui.typewrite(uh)
     pyautogui.press("enter")
+    if keyboard.is_pressed('esc'):
+        break
