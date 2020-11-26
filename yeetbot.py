1c4253099fa524bda0e69d927bce257fc0022a9c
Myrrh Khan <myrrhkhan@gmail.com>
2020-11-25 19:56:15 -0500

    added user input

--- yeetbot.py
+++ yeetbot.py
@@ -1,8 +1,16 @@
 import pyautogui, time, keyboard
-time.sleep(5)
+while True:
+    try: 
+        headstart = int(input("enter how many seconds you need to get on your app? "))
+        messagedelay = int(input("enter delay between messages, in seconds: "))
+        print("you have %s seconds" %headstart)
+        time.sleep(headstart)
+    except: continue
+    else: break
 yeet = open("beemovie.txt", "r")
 for uh in yeet:
     pyautogui.typewrite(uh)
     pyautogui.press("enter")
+    time.sleep(messagedelay)
     if keyboard.is_pressed('esc'):
         break
